3666ccba281298b86787efa810239c84af4c95b0
dooco <padraigdooley@gmail.com>
2022-10-06 06:02:41 +0000

    test overlap check routine

--- run.py
+++ run.py
@@ -13,11 +13,12 @@ ALPHA_NUMERO = {
     "H": 7
 }
 ONE_TO_EIGHT = ("1", "2", "3", "4", "5", "6", "7", "8")
-board_size = 8
-PLAYER_BOARD = [[" "]*8 for _ in range(board_size)]
-PLAYER_BOARD_GUESS = [[" "]*8 for _ in range(board_size)]
-COMPUTER_BOARD = [[" "]*8 for _ in range(board_size)]
-COMPUTER_BOARD_GUESS = [[" "]*8 for _ in range(board_size)]
+BOARD_SIZE = 8
+PLAYER_BOARD = [[" "]*8 for _ in range(BOARD_SIZE)]
+print(len(PLAYER_BOARD))
+PLAYER_BOARD_GUESS = [[" "]*8 for _ in range(BOARD_SIZE)]
+COMPUTER_BOARD = [[" "]*8 for _ in range(BOARD_SIZE)]
+COMPUTER_BOARD_GUESS = [[" "]*8 for _ in range(BOARD_SIZE)]
 SHIPS = [2,3,3,4,5]
 
 
@@ -44,8 +45,8 @@ def place_ships(board):
         while True:
             if board == COMPUTER_BOARD:
                 direction = random.choice(["H", "V"])
-                row = random.randint(0, board_size-1)
-                col = random.randint(0, board_size-1)
+                row = random.randint(0, BOARD_SIZE-1)
+                col = random.randint(0, BOARD_SIZE-1)
                 if make_sure_ship_fits(board, ship, direction, row, col):
                     if not no_ship_overlap(board, ship, direction, row, col):
                         print(direction, row, col)
@@ -59,17 +60,18 @@ def place_ships(board):
             else:
                 print(f"Place your ship size: {ship} on grid")
                 direction, row, col = get_players_ship_position()
+                print(direction, row, col)
                 if make_sure_ship_fits(board, ship, direction, row, col):
                     if not no_ship_overlap(board, ship, direction, row, col):
                         if direction == "H":
                             for i in range(col, col + ship):
                                 print(row, i)
                                 board[row][i] = "X"
-                            else:
-                                for i in range(row, row + ship):
-                                    board[i][col] = "X"
-                            break
-                break
+                        else:
+                            for i in range(row, row + ship):
+                                board[i][col] = "X"
+                        break
+#                break
             
 
 def make_sure_ship_fits(board, ship, direction, row, col):
@@ -78,26 +80,28 @@ def make_sure_ship_fits(board, ship, direction, row, col):
     """
     if board == COMPUTER_BOARD:
         if direction == "H":
-            if col + ship > board_size:
+            if col + ship > (BOARD_SIZE - 1):
                 return False
             else:
                 return True
         else:
-            if row + ship > board_size:
+            if row + ship > (BOARD_SIZE - 1):
                 return False
             else:
                 return True
     else:
         if direction == "H":
-            if col + ship > board_size:
+            if col + ship > (BOARD_SIZE - 1):
                 print("The ship cannot go of the board\nTry another location\n")
                 return False
             else:
                 return True
         else:
-            if row + ship > board_size:
+            if row + ship > (BOARD_SIZE - 1):
                 print("The ship cannot go of the board\nTry another location\n")
                 return False
+            else:
+                return True
 
 
 
@@ -116,22 +120,30 @@ def no_ship_overlap(board, ship, direction, row, col):
                     return True
         return False
     else:
-        print("player board overlap check")
-
+        if direction == "H":
+            for i in range(col, col + ship):
+                if board[row][i] == "X":
+                    print("Wrong entry, ship cannot go ever another\nTry again\n")
+                    return True
+        else:
+            for i in range(row, row + ship):
+                if board[i][col] == "X":
+                    print("Wrong entry, ship cannot go over another\nTry again\n")
+                    return True
         return False
 
 
 def get_players_ship_position():
     while True:
         try:
-            direction = input("What direction would you like to position your ship, (H) horizontal (V) Vertical\n").upper()
+            direction = input("Enter direction you would like to position your ship, (H) horizontal (V) Vertical\n").upper()
             while direction not in ("H", "V"):
                 print("Incorrect choice, only H or V")
                 break
-            if direction =="H" or direction == "V":
+            if direction == "H" or direction == "V":
                 break
         except ValueError:
-            print("Wrong nput Try 'H' or 'V'\n")
+            print("Wrong input Try 'H' or 'V'\n")
     while True:
         try:
             row = input(("Enter row you would like to place ship (1-8) \n"))
@@ -142,11 +154,10 @@ def get_players_ship_position():
                 row = int(row) - 1
                 break
         except ValueError:
-            print("Wrong input, select a number between 1 and 8\n ")
-
+            print("Wrong input, select a number between 1 and 8\n")
     while True:
         try:
-            col = input("Enter column you would like to place ship (A to H\n").upper()
+            col = input("Enter column you would like to place ship (A to H)\n").upper()
             while col not in ALPHA_NUMERO.keys():
                 print("Wrong input, enter A to H\n")
                 break
@@ -158,101 +169,8 @@ def get_players_ship_position():
     return direction, row, col
 
 
-scores = {"computer": 0, "player": 0}
+# scores = {"computer": 0, "player": 0}
 
-
-class Board:
-    """
-    Board class sets board size, number of ships, player's name 
-    and the board type: player or computer
-    """
-
-    def __init__(self, size, num_ships, name, type):
-        self.size = size
-        self.board = [["." for x in range(size)] for y in range(size)]
-        self.num_ships = num_ships
-        self.name = name
-        self.type = type
-        self.guesses = []
-        self.ships = []
-
-    def print(self):
-        for row in self.board:
-            print(" ".join(row))
-
-    def guess(self, x, y):
-        self.guesses.append((x,y))
-        self.board[x][y] = "X"
-
-        if (x,y) in self.ships:
-            self.board[x][y] = "*"
-            return "Hit"
-        else:
-            return "Miss"
-
-    def add_ship(self, x, y):
-        if len(self.ships) >= self.num_ships:
-            print("Error: you cannot add any more ships!")
-        else:
-            self.ships.append((x,y))
-            if self.type == "player":
-                self.board[x][y] = "@"
-    
-    def populate_board(size):
-        """
-        Position ships on grid in random positions
-        """
-    
-        x, y = rand_coordinates(size)
-        while (x, y) in self.ships:
-            x, y = rand_coordinates(self.size)
-        self.add_ship(x,y)
-
-
-def random_point(size):
-    """
-    function to return random integer between 0 and size
-    """
-
-    return randint(0, size - 1)
-
-def rand_coordinates(size):
-    """
-    creates two random numbers within the range 0 and board size
-    """
-    randomized = randint(0, size)
-    print((randomized))
-    return randint(0, size-1), randint(0, size-1)
-
-def valid_coordinates(x, y, size):
-    """
-    Verifies guess co-ordinates entered by player
-    """
-    return ((0 <= x < size) and (0 <= y < size))
-    
-
-
-        
-
-
-
-
-def make_guess():
-    """
-    Get x and y guess co-ordinates from player
-    """
-    while True:
-        try:
-            print("=" * 58)
-            x = int(input(f"Enter row in range 0 - {size - 1}: \n"))
-            y = int(input(f"Enter column in range 0 - {size - 1}: \n"))
-            break
-        except ValueError:
-            print("Error, Row and Column must be valid numbers\n")
-    return(x, y)
-    
-
-   
 
 
 def welcome():
@@ -274,33 +192,12 @@ def welcome():
     print("* = co-ordinates of a HIT")
     print("X = co-ordinates of a MISS")
 
-def play_game(computer_board, Player_board):
-    return True #to get code to work
-
 
 def new_game():
     """
     Starts a new game, set board size, number of ships,
     resets scores and initialises boards.
     """
-    size = 5
-    num_ships = 4
-    scores["computer"] = 0
-    scores["player"] = 0
-    print("=" * 58)
-    print(" Welcome to BATTLESHIPS!!")
-    print(f" Board Size: {size}. Number of ships: {num_ships}")
-    print(" Top left corner is row: 0, col: 0")
-    print("=" * 58)
-    print("=" * 24 + " Legend " + "=" * 24)
-    player_name = input(" Please enter your name:\n")
-    print("=" * 58)
-
-    
-    #computer_board = Board(size, num_ships, "Computer", type="computer")
-    
-    #player_board = Board(size, num_ships, player_name, type="player")
-
     board = COMPUTER_BOARD
     place_ships(board)
     show_board(COMPUTER_BOARD)
@@ -310,23 +207,7 @@ def new_game():
     
 
 
-    for _ in range(num_ships):
-        populate_board(player_board)
-        populate_board(computer_board)
-    
-    computer_board.print()
 
-    print("="* 58)
-    
-    Player_board.print()
-    #play_game(computer_board, Player_board)
     
 new_game()
-size = 5
-x, y = make_guess()
-if valid_coordinates(x, y, size):
-    print("co-ordinitates are within range")
-
-
-
 
